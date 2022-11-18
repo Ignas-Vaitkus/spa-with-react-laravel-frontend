2a9b3262c67a55048f71f78eec7081abf29cf963
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-11-18 14:42:20 +0200

    Employee rendering

--- src/components/Employees/Employees.js
+++ src/components/Employees/Employees.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Employees.css';
+import React, { useEffect, useState } from "react";
+import StickyTable from "../StickyTable/StickyTable";
+import "./Employees.css";
 
-const Employees = () => (
-  <div className="Employees">
-    Employees Component
-  </div>
-);
+const columns = [
+  { id: "first_name", label: "First Name", minWidth: 170 },
+  { id: "last_name", label: "Last Name", minWidth: 300 },
+  { id: "actions", label: "", minWidth: 30 },
+];
 
-Employees.propTypes = {};
+const Employees = ({ callback }) => {
+  const [projects, setProjects] = useState([{ id: 1, name: "" }]);
 
-Employees.defaultProps = {};
+  useEffect(() => {
+    callback("http://127.0.0.1:8000/api/employees", setProjects).catch(
+      console.error
+    );
+
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  console.log(projects);
+
+  return (
+    <div className="Projects">
+      <StickyTable
+        {...{
+          columns: columns,
+          rows: projects,
+        }}
+      />
+    </div>
+  );
+};
 
 export default Employees;
