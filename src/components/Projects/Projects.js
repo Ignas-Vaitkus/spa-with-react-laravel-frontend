323e01b5ba0ba7dd483c79d17b5f41cf8de91bdf
ignas-vaitkus <ignas.vaitkus7@gmail.com>
2022-11-19 15:18:42 +0200

    Add new project functionality

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,3 +1,4 @@
+import { Button, Card, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import StickyTable from "../StickyTable/StickyTable";
 import "./Projects.css";
@@ -8,18 +9,42 @@ const columns = [
   { id: "actions", label: "", minWidth: 30 },
 ];
 
-const Projects = ({ callback }) => {
-  const [projects, setProjects] = useState([{ id: 1, name: "" }]);
+const Projects = ({ get, post, put, patch, del }) => {
+  const [projects, setProjects] = useState([
+    { id: 1, name: "", employees_names: "" },
+  ]);
+
+  const [newProject, setNewProject] = useState("");
 
   useEffect(() => {
-    callback("http://127.0.0.1:8000/api/project-employee", setProjects).catch(
+    get("http://127.0.0.1:8000/api/project-employee", setProjects).catch(
       console.error
     );
 
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  console.log(projects);
+  const addProjectHandler = async (event) => {
+    event.preventDefault();
+
+    console.log({
+      name: newProject,
+    });
+
+    await post("http://127.0.0.1:8000/api/projects", {
+      name: newProject,
+    }).catch(console.error);
+
+    await get("http://127.0.0.1:8000/api/project-employee", setProjects).catch(
+      console.error
+    );
+
+    setNewProject("");
+  };
+
+  const changeHandler = (event) => {
+    setNewProject(event.target.value);
+  };
 
   return (
     <div className="Projects">
@@ -29,6 +54,23 @@ const Projects = ({ callback }) => {
           rows: projects,
         }}
       />
+
+      <form onSubmit={addProjectHandler}>
+        <Card
+          sx={{ p: 2, m: 1, display: "flex", alignItems: "center", gap: 5 }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="New Project"
+            variant="outlined"
+            value={newProject}
+            onChange={changeHandler}
+          />
+          <Button variant="outlined" type="submit" sx={{}}>
+            Submit
+          </Button>
+        </Card>
+      </form>
     </div>
   );
 };
